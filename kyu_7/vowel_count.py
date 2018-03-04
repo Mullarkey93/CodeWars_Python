eb422522096e78b3a734b3a1ccf2ff5bbbc46e9b
DESKTOP-FHHFM48\Michael <michael9389@gmail.com>
2018-03-04 21:41:18 +0000

    update answer

--- kyu_7/vowel_count.py
+++ kyu_7/vowel_count.py
@@ -11,3 +11,6 @@ def getCount(inputStr):
     for c in list(inputStr):
         if c in vowels: num_vowels += 1
     return num_vowels
+
+def getCount2(inputStr):
+    return sum(1 for let in inputStr if let in "aeiouAEIOU")
