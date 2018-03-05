f7bc474bbe4e6b0cfdbd7968303d56a62326ebd1
DESKTOP-FHHFM48\Michael <michael9389@gmail.com>
2018-03-05 21:23:22 +0000

    new structure for kyu_7

---
+++ src/kyu_7/test_vowel_count.py
@@ -0,0 +1,10 @@
+from src.kyu_7.vowel_count import getCount
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_vowel_count(self):
+        self.assertEqual(getCount("abracadabra"), 5)
+
+if __name__ == '__main__':
+    unittest.main()
