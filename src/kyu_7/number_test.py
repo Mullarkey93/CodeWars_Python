182b937a238acfdf5cc6fdda6406613572bc9dc7
Mullarkey93 <michael9389@gmail.com>
2019-11-17 19:31:54 +0000

    chore: new challenge kyu 7

---
+++ src/kyu_7/number_test.py
@@ -0,0 +1,11 @@
+from src.kyu_7.number import number
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_vowel_count(self):
+        self.assertEqual(number([]), [])
+        self.assertEqual(number(["a", "b", "c"]), ["1: a", "2: b", "3: c"])
+
+if __name__ == '__main__':
+    unittest.main()
